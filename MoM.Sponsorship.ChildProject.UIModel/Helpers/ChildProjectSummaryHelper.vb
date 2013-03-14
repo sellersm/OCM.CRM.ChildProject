e0c51230b940beadf9b8f29f1eb2dbe1daad120a
Cary Mayeda <cmayeda@ba.org>
2013-03-14 01:32:44 -0600

    Added CLOSUREDATE - FogBugz 1029

--- MoM.Sponsorship.ChildProject.UIModel/Helpers/ChildProjectSummaryHelper.vb
+++ MoM.Sponsorship.ChildProject.UIModel/Helpers/ChildProjectSummaryHelper.vb
@@ -10,6 +10,7 @@ Imports MoM.Common
 ''' <remarks></remarks>
 Public NotInheritable Class ChildProjectSummaryHelper
     Private Const errorTextCouldNotFindChildProjectTypeOther = "Could not find 'Other' Project Type code table value."
+    Private Const errorTextCouldNotFindChildProjectStatusCodeClosed = "Could not find 'Closed' Project Status code table value."
 
     Private Property model As UIModeling.Core.RootUIModel = Nothing
 
@@ -17,6 +18,7 @@ Public NotInheritable Class ChildProjectSummaryHelper
 
     ' Code Table IDs
     Property childProjectTypeOtherGuid As String = ""
+    Property childProjectStatusCodeClosedGuid As String = ""
 
 
     Public Sub New(ByVal uiModel As UIModeling.Core.RootUIModel, ByVal mode As CRMHelper.FormMode)
@@ -25,22 +27,59 @@ Public NotInheritable Class ChildProjectSummaryHelper
 
         'Add all the handlers
         AddHandler model.Fields(ChildProjectFields.CHILDPROJECTTYPECODEID).ValueChanged, AddressOf ChildProjectTypeCodeID_ValueChanged
+        If Me.helperMode <> CRMHelper.FormMode.View Then
+            AddHandler model.Fields(ChildProjectFields.CHILDPROJECTSTATUSCODEID).ValueChanged, AddressOf ChildProjectStatusCodeID_ValueChanged
+        End If
     End Sub
 
     Public Sub InitializeCodeTableVars()
         'Initialize Code Table IDs 
         Using crmSQLConnection = model.GetRequestContext().OpenAppDBConnection()
             childProjectTypeOtherGuid = CRMHelper.GetCodeTableItemID(crmSQLConnection, CodeTableFields.CHILDPROJECTTYPE_OTHER, True, errorTextCouldNotFindChildProjectTypeOther)
+            childProjectStatusCodeClosedGuid = CRMHelper.GetCodeTableItemID(crmSQLConnection, CodeTableFields.CHILDPROJECTSTATUS_CLOSED, True, errorTextCouldNotFindChildProjectStatusCodeClosed)
         End Using
     End Sub
 
     Public Sub InitializeUIDisplay()
         ' Set the UI based on loaded values
         Toggle_ChildProjectTypeOther()
+        Toggle_ChildProjectClosureDate()
+
     End Sub
 
     Private Sub ChildProjectTypeCodeID_ValueChanged(ByVal sender As Object, ByVal e As Blackbaud.AppFx.UIModeling.Core.ValueChangedEventArgs)
         Toggle_ChildProjectTypeOther()
+    End Sub
+
+    Private Sub ChildProjectStatusCodeID_ValueChanged(ByVal sender As Object, ByVal e As Blackbaud.AppFx.UIModeling.Core.ValueChangedEventArgs)
+        Toggle_ChildProjectClosureDate()
+    End Sub
+
+    Private Sub Toggle_ChildProjectClosureDate()
+        ' View form doesn't change based on the Project Status
+        If Me.helperMode <> CRMHelper.FormMode.View Then
+
+            If model.Fields(ChildProjectFields.CHILDPROJECTSTATUSCODEID).ValueObject Is Nothing OrElse
+                model.Fields(ChildProjectFields.CHILDPROJECTSTATUSCODEID).ValueObject.ToString <> childProjectStatusCodeClosedGuid Then
+
+                'model.Fields(ChildProjectFields.CLOSUREDATE).Visible = False
+                model.Fields(ChildProjectFields.CLOSUREDATE).ValueObject = Nothing
+
+                If helperMode = CRMHelper.FormMode.Add Or helperMode = CRMHelper.FormMode.Edit Then
+                        model.Fields(ChildProjectFields.CLOSUREDATE).Enabled = False
+                        model.Fields(ChildProjectFields.CLOSUREDATE).Required = False
+                End If
+
+            Else
+                'model.Fields(ChildProjectFields.CLOSUREDATE).Visible = True
+
+                If helperMode = CRMHelper.FormMode.Add Or helperMode = CRMHelper.FormMode.Edit Then
+                        model.Fields(ChildProjectFields.CLOSUREDATE).Enabled = True
+                        model.Fields(ChildProjectFields.CLOSUREDATE).Required = True
+                End If
+            End If
+        End If
+
     End Sub
 
     Private Sub Toggle_ChildProjectTypeOther()
